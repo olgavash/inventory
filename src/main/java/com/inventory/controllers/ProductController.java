85c7d53c13b66f6cf727c851830f6680c1b3809c
zoloto <zolo@yandex.ru>
2019-11-11 19:35:05 -0500

    renamed from ProductList to Product, added hello part, adjusted views

--- src/main/java/com/inventory/controllers/ProductController.java
+++ src/main/java/com/inventory/controllers/ProductController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
@@ -18,14 +18,32 @@ public class ProductController {
     @Autowired
     private ProductClassDao productClassDao;
 
-    @RequestMapping(value = "")
+    @RequestMapping("/")
     public String index(Model model) {
+
+        model.addAttribute("product", productDao.findAll());
+        model.addAttribute("title", "Cafeteria Inventory");
+
+        return "index";
+    }
+
+    @RequestMapping("/product")
+    public String showProducts(Model model) {
 
         model.addAttribute("product", productDao.findAll());
         model.addAttribute("title", "Product List");
 
-        return "product/index";
+        return "product/products";
     }
 
+    @RequestMapping(value="/addProduct", method = RequestMethod.GET)
+    public String addProductForm(Model model) {
+        return "product/addProduct";
+    }
 
+    @RequestMapping(value="/addProduct", method = RequestMethod.POST)
+    public String addProduct(Model model) {
+        // Method to handle post addProduct
+        return "product/products";
+    }
 }
