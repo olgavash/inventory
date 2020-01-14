36f6e371063f0b09e5fdd1f41b487bfa7304d5e2
zoloto <zolo@yandex.ru>
2020-01-13 20:55:35 -0500

    1/13/20

--- src/main/java/com/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/InventoryApplication.java
@@ -2,9 +2,11 @@ package com.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan
 public class InventoryApplication {
 
 	public static void main(String[] args) {
